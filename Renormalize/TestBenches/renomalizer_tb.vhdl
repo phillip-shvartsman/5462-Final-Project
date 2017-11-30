2b2c3d23fd88f2b2d2f46fe5345b327b67d050d0
gavinnguyenece <nguyen.1331@osu.edu>
2017-11-29 22:49:16 -0500

    Update renomalizer_tb.vhdl

--- Renormalize/TestBenches/renomalizer_tb.vhdl
+++ Renormalize/TestBenches/renomalizer_tb.vhdl
@@ -70,10 +70,10 @@ end renorm_test_vect;
 
 package body renorm_test_vect is
 
-procedure gen_vec (SIGNAL aid_sig,bid_sig,resid_sig : OUT string (1 to 6);
-               SIGNAL aval,bval,exp_res : OUT std_logic_vector (31 downto 0);
+procedure gen_vec (SIGNAL iid_sig,resid_sig : OUT string (1 to 6);
+               SIGNAL ival : std_logic_vector (59 downto 0);
+	       SIGNAL exp_res : std_logic_vector (31 downto 0);
                SIGNAL C : IN std_logic_vector (31 downto 0);
-               SIGNAL latch,drive : OUT std_ulogic;
                SIGNAL err_sig : OUT bit;
                SIGNAL score : OUT integer) is
 variable cur_line : LINE;
@@ -85,7 +85,6 @@ variable aid,bid,resid : string (1 to 6);
 variable num_tests,num_errors : integer := 0;
 Begin
   aval <= HIGHZ; bval <= HIGHZ; exp_res <= HIGHZ;
-  latch <= '1'; drive <= '1';
   WHILE (NOT ENDFILE(test_data)) LOOP
     --get next input test vector and expected result
     readline(test_data,cur_line);
@@ -95,15 +94,11 @@ Begin
     std_result_val := To_StdLogicVector(result_val);
     num_tests := num_tests + 1;
     -- run through bus cycle to send data to unit
-    aid_sig <= "======", aid after 20 ns; 
-    bid_sig <= "======", bid after 20 ns; 
+    iid_sig <= "======", aiid after 20 ns; 
     resid_sig <= "======", resid after 20 ns;
     -- drive signals on bus
-    aval <= To_StdLogicVector(a_test_val) after 20 ns, HIGHZ after 80 ns;
-    bval <= To_StdLogicVector(b_test_val) after 20 ns, HIGHZ after 80 ns;
-    latch <= '0' after 20 ns, '1' after 70 ns;
+    ival <= To_StdLogicVector(in_test_val) after 20 ns, HIGHZ after 80 ns;
     wait for 100 ns;
-    drive <= '0' after 20 ns, '1' after 80 ns;
     exp_res <= std_result_val after 20 ns, HIGHZ after 80 ns;
     wait for 50 ns;
     ASSERT (C = std_result_val)
