ea4c3f133aa3c11d6fb969aeaa97110b95aa281d
priyankasistla <34215632+priyankasistla@users.noreply.github.com>
2017-12-05 13:16:58 -0500

    Adding the new tcl

--- fpuMultiplierStage/Testbenchs/fpuMultiplierStageTestbenchs.tcl
+++
@@ -1,2 +0,0 @@
-vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/cmaMultiplier_tb.vhdl
-vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/boothMultiplier_tb.vhdl
